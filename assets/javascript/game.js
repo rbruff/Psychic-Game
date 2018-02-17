b72ab09f2ba636dcbd09d8bc0fa7c8cecfa4df45
renee <reneebruff@gmail.com>
2018-02-17 03:45:28 -0500

    js uptil loop

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// HTML element variables
+var winsHTML = document.getElementById('wins');
+var lossesHTML = document.getElementById('losses');
+var guessesLeftHTML = document.getElementById('guesses-left');
+var guessesSfHTML = document.getElementById('guesses-sf');
+
+
+// array of possible choices
+var choices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// variable for wins and loses
+var wins = 0;
+var losses = 0;
+
+// variable for guesses left
+var guessesLeft = 9;
+
+// key pressed function
+document.onkeydown = function (event) {
+    // capture user guess
+    var guessesSf = event.key;
+    // create computer letter
+    var compLetter = choices[Math.floor(Math.random() * choices.length)];
+    // compare user guess vs comp letter... loop??
+
+}
+
