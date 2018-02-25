e13e21313c82734cf9140c0a272c61c2676973ca
renee <reneebruff@gmail.com>
2018-02-25 17:36:04 -0500

    js not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,16 @@
-// HTML element variables
-var winsHTML = document.getElementById('wins');
-var lossesHTML = document.getElementById('losses');
-var guessesLeftHTML = document.getElementById('guesses-left');
-var guessesSfHTML = document.getElementById('guesses-sf');
+// // HTML element variables
+// var winsHTML = document.getElementById('wins');
+// var lossesHTML = document.getElementById('losses');
+// var guessesLeftHTML = document.getElementById('guesses-left');
+// var guessesSfHTML = document.getElementById('guesses-sf');
 
 
 // array of possible choices
-var choices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // variable for wins and loses
-var wins = 0;
-var losses = 0;
+var winScore = 0;
+var lossScore = 0;
 
 // variable for guesses left
 var guessesLeft = 9;
@@ -21,7 +21,25 @@ document.onkeydown = function (event) {
     var guessesSf = event.key;
     // create computer letter
     var compLetter = choices[Math.floor(Math.random() * choices.length)];
-    // compare user guess vs comp letter... loop??
+    console.log(compLetter)
+    // compare user guess vs comp letter
+    if (guessesSf === compLetter) {
+        winScore++;
+        guessesLeft = 9;
+        guessesSf = ""
+
+    } else {
+        guessesLeft--;
+        document.getElementById("guess-sf").innerHTML = document.getElementById("guess-sf").innerHTML + ", " + guessesSf;
+        if (guessesLeft === 0) {
+            lossScore++;
+            guessesLeft = 9;
+            guessesSf = ""
+        }
+    }
+    // write data to html
+    document.getElementById("wins").innerHTML = winScore;
+    document.getElementById("losses").innerHTML = lossScore;
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
 
 }
-
